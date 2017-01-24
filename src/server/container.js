990badef271fc77c114f9626dcbc411f3df0c8b6
Janos Hardi <janos.hardi@gawker.com>
2017-01-24 11:23:51 +0100

    fix(cache): usie correct path

--- src/server/container.js
+++ src/server/container.js
@@ -5,6 +5,6 @@ di.registerModule(require('lab-config'), 'config');
 di.registerModule(require('lab-config/implementations/memory'), 'config-memory');
 
 di.registerModule(require('./cache'), 'cache');
-di.registerModule(require('./cache/implemenetations/memory'), 'cache-memory');
+di.registerModule(require('./cache/implementations/memory'), 'cache-memory');
 
 export default di;
