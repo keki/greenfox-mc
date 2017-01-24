5e6612aff96b83f5b969da7f14e2e116ebc114bc
Janos Hardi <janos.hardi@gawker.com>
2017-01-24 13:27:30 +0100

    feat(cache/redis): initial add of redis cache implementation

--- src/server/container.js
+++ src/server/container.js
@@ -6,5 +6,6 @@ di.registerModule(require('lab-config/implementations/memory'), 'config-memory')
 
 di.registerModule(require('./cache'), 'cache');
 di.registerModule(require('./cache/implementations/memory'), 'cache-memory');
+di.registerModule(require('./cache/implementations/redis'), 'cache-redis');
 
 export default di;
