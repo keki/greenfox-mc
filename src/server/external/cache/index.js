f55e621a907556ccf98ebe966c5b3be01819a15c
Oliver Kovacs <oliver@kinja.com>
2017-02-01 11:46:14 +0100

    fix(server/external/cache): Default to memory implementation.

--- src/server/external/cache/index.js
+++ src/server/external/cache/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function Cache(container) {
-  const implementation = container.get('config').get(Cache.serviceName);
+  const implementation = container.get('config').get(Cache.serviceName) || 'memory';
   return container.getImplementation(Cache.serviceName, implementation);
 }
 
